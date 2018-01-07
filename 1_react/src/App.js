5d70ceb358fa3638a00f395d1cdce18229f79fc6
bussierem <max.bussiere@gmail.com>
2018-01-07 12:43:10 -0600

    App in functional state - still missing persistent data handling

--- 1_react/src/App.js
+++ 1_react/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, ListGroup, ListGroupItem, Badge, Progress } from 'reactstrap';
+
 import data from './projects.json';
+
+var jsonfile = require('jsonfile');
 
 class ProjectList extends Component {
   constructor(props) {
@@ -9,16 +12,47 @@ class ProjectList extends Component {
     this.state = {
       projects: data.projects
     };
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+  }
+
+  saveData() {
+    console.error('TODO:  IMPLEMENT SAVING');
+  }
+
+  addHoursToProject(name, hours) {
+    var idx = this.state.projects.findIndex((p) => {
+      return p.id === parseInt(name);
+    });
+    var projectList = this.state.projects;
+    projectList[idx].project.hours += hours;
+    this.setState({projects: projectList});
+    this.saveData();
+  }
+
+  add(e) {
+    this.addHoursToProject(e.target.name, 1);
+  }
+
+  subtract(e) {
+    this.addHoursToProject(e.target.name, -1);
   }
 
   render() {
-    const projectList = this.state.projects.map((project) => (
-      <li id="project">
-        Week {project.week} ({project.dateRange}): {project.project}  [Hours Spent: {project.hours}]
-      </li>
+    const projectList = this.state.projects.map((p) => (
+      <ListGroupItem key={p.id.toString()}>
+        Week {p.project.week} ({p.project.dateRange}): {p.project.desc}
+        <Badge pill>{p.project.hours}</Badge>
+        <Button outline color="primary" size="sm" name={p.id} onClick={this.add}>+</Button>
+        <Button outline color="secondary" size="sm" name={p.id} onClick={this.subtract}>-</Button>
+        <br />
+        <Progress striped value={(parseInt(p.project.hours) / 8) * 100}>{p.project.hours}</Progress>
+      </ListGroupItem>
     ));
     return (
-      <ol id="projectList">{projectList}</ol>
+      <div className="List">
+        <ListGroup>{projectList}</ListGroup>
+      </div>
     )
   }
 }
