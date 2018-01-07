f3a91f9412cf531362f06a85d3617d3f581fd6d9
bussierem <max.bussiere@gmail.com>
2018-01-07 14:53:13 -0600

    adding projects now works - still no persistence...

--- 1_react/src/App.js
+++ 1_react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, ListGroup, ListGroupItem, Badge, Progress } from 'reactstrap';
+import { Form, Label, Input, Button, ListGroup, ListGroupItem, Badge, Progress } from 'reactstrap';
 
 import data from './projects.json';
 
@@ -16,8 +16,23 @@ class ProjectList extends Component {
     this.subtract = this.subtract.bind(this);
   }
 
+  componentDidMount() {
+    this.TimerId = setInterval(
+      () => {this.setState({projects: data.projects})},
+      500
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.TimerID);
+  }
+
   saveData() {
     console.error('TODO:  IMPLEMENT SAVING');
+  }
+
+  updateData() {
+    this.setState({projects: data.projects});
   }
 
   addHoursToProject(name, hours) {
@@ -41,19 +56,76 @@ class ProjectList extends Component {
   render() {
     const projectList = this.state.projects.map((p) => (
       <ListGroupItem key={p.id.toString()}>
-        Week {p.project.week} ({p.project.dateRange}): {p.project.desc}
+        Week {p.project.week}:  {p.project.desc}
         <Badge pill>{p.project.hours}</Badge>
         <Button outline color="primary" size="sm" name={p.id} onClick={this.add}>+</Button>
         <Button outline color="secondary" size="sm" name={p.id} onClick={this.subtract}>-</Button>
-        <br />
         <Progress striped value={(parseInt(p.project.hours) / 8) * 100}>{p.project.hours}</Progress>
       </ListGroupItem>
     ));
     return (
       <div className="List">
-        <ListGroup>{projectList}</ListGroup>
+        <ListGroup>
+          {projectList}
+          <ListGroupItem key="999">
+            <AddProject />
+          </ListGroupItem>
+        </ListGroup>
       </div>
     )
+  }
+}
+
+class AddProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false,
+      value: ''
+    };
+    this.changed = this.changed.bind(this);
+    this.submit = this.submit.bind(this);
+    this.addProject = this.addProject.bind(this);
+  }
+
+  changed(e) {
+    this.setState({value: e.target.value});
+  }
+
+  submit() {
+    var newId = Math.max(...data.projects.map((p) => { return p.id; })) + 1;
+    var newProject = {
+      "id": newId,
+      "project": {
+        "week": (newId + 1).toString(),
+        "desc": this.state.value,
+        "hours": 0
+      }
+    }
+    data.projects.push(newProject);
+    this.setState({showForm: !this.state.showForm})
+  }
+
+  addProject(e) {
+    e.preventDefault();
+    this.setState({showForm: !this.state.showForm});
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.showForm &&
+          <div className="Form">
+            <Form inline>
+              <Label for="project">Project:</Label>
+              <Input type="text" name="project" id="project" onChange={this.changed}/>
+              <Button onClick={this.submit}>Add Project</Button>
+            </Form>
+          </div>
+        }
+        <Button disabled={this.state.showForm} onClick={this.addProject}>Add Project</Button>
+      </div>
+    );
   }
 }
 
