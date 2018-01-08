f0a937de46beb75fe2a1aa927c249df79c6a6ee9
bussierem <max.bussiere@gmail.com>
2018-01-07 20:41:09 -0600

    done, wooooh.  Messy-ish code, but React App does everything I want it to (for now)

--- 1_react/src/App.js
+++ 1_react/src/App.js
@@ -2,47 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import { Form, Label, Input, Button, ListGroup, ListGroupItem, Badge, Progress } from 'reactstrap';
 
-import data from './projects.json';
-
-var jsonfile = require('jsonfile');
-
 class ProjectList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      projects: data.projects
-    };
+    this.state = {};
     this.add = this.add.bind(this);
     this.subtract = this.subtract.bind(this);
+    this.delete = this.delete.bind(this);
   }
 
   componentDidMount() {
-    this.TimerId = setInterval(
-      () => {this.setState({projects: data.projects})},
-      500
-    );
   }
 
   componentWillUnmount() {
-    clearInterval(this.TimerID);
   }
 
   saveData() {
-    console.error('TODO:  IMPLEMENT SAVING');
-  }
-
-  updateData() {
-    this.setState({projects: data.projects});
   }
 
   addHoursToProject(name, hours) {
-    var idx = this.state.projects.findIndex((p) => {
-      return p.id === parseInt(name);
-    });
-    var projectList = this.state.projects;
-    projectList[idx].project.hours += hours;
-    this.setState({projects: projectList});
-    this.saveData();
+    this.props.onBtnClick(name, hours);
   }
 
   add(e) {
@@ -53,23 +32,25 @@ class ProjectList extends Component {
     this.addHoursToProject(e.target.name, -1);
   }
 
+  delete(e) {
+    this.props.onDelete(e.target.name);
+  }
+
   render() {
-    const projectList = this.state.projects.map((p) => (
-      <ListGroupItem key={p.id.toString()}>
+    var projectList = this.props.projects.map((p) => (
+      <ListGroupItem key={p.id.toString()} >
+        <Button color="danger" size="sm" name={p.id.toString()} onClick={this.delete}>X</Button>
         Week {p.project.week}:  {p.project.desc}
         <Badge pill>{p.project.hours}</Badge>
-        <Button outline color="primary" size="sm" name={p.id} onClick={this.add}>+</Button>
-        <Button outline color="secondary" size="sm" name={p.id} onClick={this.subtract}>-</Button>
-        <Progress striped value={(parseInt(p.project.hours) / 8) * 100}>{p.project.hours}</Progress>
+        <Button outline color="primary" size="sm" name={p.id.toString()} onClick={this.add}>+</Button>
+        <Button outline color="secondary" size="sm" name={p.id.toString()} onClick={this.subtract}>-</Button>
+        <Progress striped value={(parseInt(p.project.hours, 10) / 8) * 100}>{p.project.hours}</Progress>
       </ListGroupItem>
     ));
     return (
       <div className="List">
         <ListGroup>
           {projectList}
-          <ListGroupItem key="999">
-            <AddProject />
-          </ListGroupItem>
         </ListGroup>
       </div>
     )
@@ -80,7 +61,6 @@ class AddProject extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showForm: false,
       value: ''
     };
     this.changed = this.changed.bind(this);
@@ -93,7 +73,7 @@ class AddProject extends Component {
   }
 
   submit() {
-    var newId = Math.max(...data.projects.map((p) => { return p.id; })) + 1;
+    var newId = Math.max(...this.props.projects.map((p) => { return p.id; })) + 1;
     var newProject = {
       "id": newId,
       "project": {
@@ -101,20 +81,19 @@ class AddProject extends Component {
         "desc": this.state.value,
         "hours": 0
       }
-    }
-    data.projects.push(newProject);
-    this.setState({showForm: !this.state.showForm})
+    };
+    this.props.onSubmit(newProject)
   }
 
   addProject(e) {
     e.preventDefault();
-    this.setState({showForm: !this.state.showForm});
+    this.props.onAddProject();
   }
 
   render() {
     return (
       <div>
-        {this.state.showForm &&
+        {this.props.showForm &&
           <div className="Form">
             <Form inline>
               <Label for="project">Project:</Label>
@@ -123,20 +102,122 @@ class AddProject extends Component {
             </Form>
           </div>
         }
-        <Button disabled={this.state.showForm} onClick={this.addProject}>Add Project</Button>
+        <Button disabled={this.props.showForm} onClick={this.addProject}>
+          Add Project
+        </Button>
       </div>
     );
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false,
+      projects: []
+    };
+    this.getProjects = this.getProjects.bind(this);
+    this.addHoursToProject = this.addHoursToProject.bind(this);
+    this.createProject = this.createProject.bind(this);
+    this.showProjectForm = this.showProjectForm.bind(this);
+    this.deleteProject = this.deleteProject.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProjects();
+  }
+
+  getProjects() {
+    var response;
+    fetch(`http://localhost:3001/projects`)
+      .then((response) => {
+        if(response.status !== 200) {
+          console.warn(response.status);
+          return;
+        }
+        response.json().then((data) => {
+          response = data;
+          this.setState({projects: data});
+        });
+      });
+    return response;
+  }
+
+  addHoursToProject(name, hours) {
+    this.setState((prevState, props) => {
+      var projectList = prevState.projects.map((p) => ({
+        ...p,
+        project: {
+          ...p.project,
+          hours: p.project.hours += ((p.id === parseInt(name, 10)) ? hours : 0),
+        },
+      }));
+      return {projects: projectList};
+    }, () => {
+      var url = `http://localhost:3001/projects/`+ name;
+      fetch(url, {
+        method: 'put',
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(this.state.projects[parseInt(name, 10)])
+      });
+    });
+  }
+
+  createProject(project) {
+    this.setState((prevState, props) => ({
+      projects: [
+        ...prevState.projects,
+        project,
+      ]
+    }), () => {
+      fetch(`http://localhost:3001/projects`, {
+        method: 'post',
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(project)
+      });
+    });
+  }
+
+  showProjectForm() {
+    this.setState({showForm: !this.state.showForm});
+  }
+
+  deleteProject(name) {
+    var idx = this.state.projects.find((p) => {
+      return name === p.id.toString();
+    });
+    this.setState((prevState, props) => {
+      prevState.projects = prevState.projects.slice(idx, 1);
+      return { projects: prevState.projects };
+    }, () => {
+      fetch(`http://localhost:3001/projects/` + name, {
+        method: 'delete',
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">New 52 Project</h1>
         </header>
-        <ProjectList />
+        <ProjectList
+          projects={this.state.projects}
+          onBtnClick={this.addHoursToProject}
+          onDelete={this.deleteProject}
+        />
+        <AddProject
+          onSubmit={this.createProject}
+          showForm={this.state.showForm}
+          projects={this.state.projects}
+          onAddProject={this.showProjectForm}
+        />
       </div>
     );
   }
