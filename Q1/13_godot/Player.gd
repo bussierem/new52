f8dfa29e3de0dec7b932f5cdfba9d255eebc593e
bussierem <max.bussiere@gmail.com>
2018-04-01 16:32:27 -0500

    week 13 done - updated project tracker

---
+++ Q1/13_godot/Player.gd
@@ -0,0 +1,49 @@
+extends Area2D
+signal hit
+
+export (int) var SPEED # player movespeed
+var screensize # size of game window
+
+func _ready():
+	screensize = get_viewport_rect().size
+	hide()
+
+func _process(delta):
+	# check for input
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	# move in a direction
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	# play animation
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+func _on_Player_body_entered( body ):
+	hide()
+	emit_signal("hit")
+	# make sure the "hit" signal isn't triggered more than once
+	$CollisionShape2D.disabled = true
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
