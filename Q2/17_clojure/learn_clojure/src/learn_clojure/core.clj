1f61f9f17e959a60e899d1ef1fe496e07062fa76
bussierem <max.bussiere@gmail.com>
2018-04-29 22:07:39 -0500

    finished week 17

--- Q2/17_clojure/learn_clojure/src/learn_clojure/core.clj
+++ Q2/17_clojure/learn_clojure/src/learn_clojure/core.clj
@@ -23,6 +23,23 @@
   )
 )
 
+(defn in [list item]
+  (prn (contains? list item))
+)
+
+(def person {
+  :first-name "Max"
+  :last-name "Bussiere"
+  :age 28
+  :occupation "Sr. Test Engineer"
+  :address {
+    :street "123 Main St"
+    :city "Milwaukee"
+    :state "WI"
+    :zipcode "53203"
+  }
+})
+
 (defn -main []
   (do (messenger))
   (do (messenger "What up"))
@@ -30,4 +47,16 @@
   (do (prnmulti '("First" "Second" "Third")))
   (do (prnLastItem [1 2 3 4 5]))
   (do (prnList '(10 :ace :jack false "Thing")))
+  (do (in #{1,2,3} 4))
+  (do (in #{1,2,3} 2))
+  (do (prn (person :occupation)))
+  (do (assoc person :spouse "Lyra Christianson"))
+  (do (prn (person :spouse)))
+  (do (prn (get person :address)))
+  (dotimes [i 3] (prn i))
+  (doseq [thing '(:max :lyra :josh)] (prn thing))
+  ; Nested looping
+  (doseq [letter [:a :b :c] number (range 3)] (prn [letter number]))
+  ; list comprehension!
+  (prn (for [letter [:a :b :c] number (range 3)] [letter number]))
 )
